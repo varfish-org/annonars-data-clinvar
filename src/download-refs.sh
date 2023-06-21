2d6fa73373f07386b6f3a1eabc4c8af4bc675a8a
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-21 15:46:52 +0200

    fix

--- src/download-refs.sh
+++ src/download-refs.sh
@@ -13,7 +13,7 @@ trap "rm -rf $TMPDIR" EXIT ERR
 if [[ ! -e $REF/hs37d5.fa.fai ]]; then
     wget -O $TMPDIR/hs37d5.fa.gz \
         https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
-    (set +e; zcat --quiet $TMPDIR/hs37d5.fa.gz) \
+    (zcat --quiet $TMPDIR/hs37d5.fa.gz || true) \
     > $REF/hs37d5.fa
     samtools faidx $REF/hs37d5.fa
 fi
@@ -21,7 +21,7 @@ fi
 if [[ ! -e $REF/GRCh38_no_alt_analysis_set.fa.fai ]]; then
     wget -O $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz \
         https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
-    (set +e; zcat --quiet $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz) \
+    (zcat --quiet $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz  || true) \
     > $REF/GRCh38_no_alt_analysis_set.fa
     samtools faidx $REF/GRCh38_no_alt_analysis_set.fa
 fi
