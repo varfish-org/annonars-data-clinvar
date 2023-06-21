d3cc86b6b60195fbe49baf7e42cc60ff3d1b8428
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-21 15:23:55 +0200

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -29,14 +29,14 @@ echo "Download reference genomes and build FAI indices"
 
 wget -O $TMPDIR/hs37d5.fa.gz \
     https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
-zcat $TMPDIR/hs37d5.fa.gz \
+zcat --quiet $TMPDIR/hs37d5.fa.gz \
 > $TMPDIR/hs37d5.fa
 rm $TMPDIR/hs37d5.fa.gz
 samtools faidx $TMPDIR/hs37d5.fa
 
 wget -O $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz \
     https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
-zcat $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz \
+zcat --quiet $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz \
 > $TMPDIR/GRCh38_no_alt_analysis_set.fa
 rm $TMPDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 samtools faidx $TMPDIR/GRCh38_no_alt_analysis_set.fa
