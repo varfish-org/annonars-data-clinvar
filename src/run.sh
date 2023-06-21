0b502100777899a48090394e15a1083dc19ed952
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-21 12:28:59 +0200

    feat: implement running clinvar-tsv and annonars-import in CI (#1)

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/bash
 
->&2 echo 'Hello world!'
+set -x
+
+pushd /tmp
+git clone https://github.com/bihealth/clinvar-tsv.git
+cd clinvar-tsv
+pip uninstall clinvar-tsv
+pip install -e .
+popd
+
+export TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+checkout_dir=$PWD
+
+echo "Download reference genomes and build FAI indices"
+
+curl https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz \
+| gzip -d -c \
+> $TMPDIR/hs37d5.fa
+samtools faidx $TMPDIR/hs37d5.fa
+
+curl ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz \
+| gzip -d -c \
+> $TMPDIR/GRCh38_no_alt_analysis_set.fa
+samtools faidx $TMPDIR/GRCh38_no_alt_analysis_set.fa
+
+echo "Run clinvar-tsv"
+
+clinvar_tsv main \
+    --b37-path $TMPDIR/hs37d5.fa \
+    --b38-path $TMPDIR/GRCh38_no_alt_analysis_set.fa \
+    --clinvar-version $(cat $checkout_dir/clinvar-release.txt)
+
+find .
