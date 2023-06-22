f3ccab3bb67fe88af3f065e843d5d4a3719b511f
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-22 08:03:22 +0200

    just one core

--- src/run.sh
+++ src/run.sh
@@ -7,6 +7,7 @@ export TMPDIR=$(mktemp -d)
 export REF=/home/runner/work/references
 
 clinvar_tsv main \
+    --cores 1 \
     --b37-path $REF/hs37d5.fa \
     --b38-path $REF/GRCh38_no_alt_analysis_set.fa \
     --clinvar-version $(cat $PWD/clinvar-release.txt || echo 2023-0617)
