55dd041c367b8c8c1df5a23e0e6059925c8a4a92
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-22 13:16:52 +0200

    wip

--- src/run-sort-strucvar.sh
+++ src/run-sort-strucvar.sh
@@ -9,11 +9,11 @@ mkdir -p ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted
 
 INPUT=${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/parsed/output.tsv
 
-cat $INPUT \
+(set +x; cat $INPUT) \
 | head -n 1 \
 > ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted/output.tsv
 
-cat $INPUT \
+(set +x; cat $INPUT) \
 | tail -n +2 \
 | sort -k2,2V -k3,3n -k4,4n -k11,11 \
 >> ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted/output.tsv
