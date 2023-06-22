e0c78e0fcdc22fb3513d1b655e427088c922cc11
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-22 14:29:04 +0200

    wip

--- src/run-annonars-import.sh
+++ src/run-annonars-import.sh
@@ -9,7 +9,7 @@ mkdir -p ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/annonars-import
 
 annonars clinvar-minimal import \
     --genome-release $(echo ${GENOME_RELEASE} | tr '[:upper:]' '[:lower:]') \
-    --path-in-tsv ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/merged/output.tsv \
+    --path-in-tsv ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/sorted/output.tsv \
     --path-out-rocksdb ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/annonars-import/rocksdb
 
 ls -lhR ${OUTPUT_DIR}
