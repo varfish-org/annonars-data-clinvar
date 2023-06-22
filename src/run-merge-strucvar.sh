f87a3686f27faad900dfea82b93c88d55f761a98
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-06-22 12:30:37 +0200

    wip

---
+++ src/run-merge-strucvar.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Run "clinvar_tsv normalize_tsv" on the output of "run-parse-xml.sh"
+
+set -euo pipefail
+set -x
+
+# TODO: remove once stable
+pushd /tmp
+/home/runner/micromamba-bin/micromamba remove -y clinvar-tsv
+git clone https://github.com/bihealth/clinvar-tsv.git
+cd clinvar-tsv
+pip install .
+popd
+
+mkdir -p ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/merged
+
+zcat ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted/output.tsv.bgz \
+| clinvar_tsv merge_tsvs \
+    --clinvar-version {params.clinvar_version} \
+    --input-tsv /dev/stdin \
+    --output-tsv /dev/stdout \
+| bgzip -c \
+> ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/merged/output.tsv.bgz
+
+tabix -S 1 -s 2 -b 3 -e 4 \
+    -f ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/merged/output.tsv.bgz
--- src/run-sort-strucvar.sh
+++ src/run-sort-strucvar.sh
@@ -7,13 +7,13 @@ set -x
 
 mkdir -p ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted
 
-INPUT=${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/parsed/output.tsv
+INPUT=${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/parsed/output.tsv
 
 cat \
     <(cat $INPUT | head -n 1) \
     <(cat $INPUT | tail -n +2 | sort -k2,2V -k3,3n -k4,4n -k11,11) \
 | bgzip -c \
-> ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/sorted/output.tsv.bgz
+> ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted/output.tsv.bgz
 
 tabix -S 1 -s 2 -b 3 -e 4 -f \
-    ${OUTPUT_DIR}/${GENOME_RELEASE}/seqvar/sorted/output.tsv.bgz
+    ${OUTPUT_DIR}/${GENOME_RELEASE}/strucvar/sorted/output.tsv.bgz
