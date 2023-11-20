f2c3a3cdf7450fecad418ebcc83e6561543ba46f
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-20 16:59:52 +0100

    feat: adding ClinVar-SV

---
+++ .github/actions/clinvar-sv-import/run.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+# Perform the annonars import
+
+set -euo pipefail
+set -x
+
+genome_release=$(echo $GENOME_RELEASE | tr '[:upper:]' '[:lower:]')
+
+mkdir -p $OUTPUT_DIR/$genome_release/clinvar-sv
+
+# The transmogrification (tr/sed) can go away after the latest clinvar-this version is used...
+export TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR" EXIT ERR
+for x in \
+  $CLINVAR_JSONL_DIR/clinvar-data-extract-vars-*/clinvar-variants-$genome_release-seqvars.jsonl.gz \
+  $CLINVAR_JSONL_DIR/clinvar-data-extract-vars-*/clinvar-variants-$genome_release-strucvars.jsonl.gz; do \
+    zcat $x \
+    | tr "'" '"' \
+    | sed -e 's/None/null/g' \
+    | egrep '"benign"|"likely benign"|"uncertain significance"|"likely pathogenic"|"pathogenic"' \
+    | egrep -v '"start": null|"stop": null' \
+    > $TMPDIR/$(basename $x .gz)
+
+    gzip -c $TMPDIR/$(basename $x .gz) > $x
+done
+
+annonars clinvar-sv import \
+    --genome-release $genome_release \
+    --path-in-jsonl $CLINVAR_JSONL_DIR/clinvar-data-extract-vars-*/clinvar-variants-$genome_release-seqvars.jsonl.gz \
+    --path-in-jsonl $CLINVAR_JSONL_DIR/clinvar-data-extract-vars-*/clinvar-variants-$genome_release-strucvars.jsonl.gz \
+    --path-out-rocksdb $OUTPUT_DIR/$genome_release/clinvar-sv/rocksdb
+
+cat >$OUTPUT_DIR/$genome_release/clinvar-sv/spec.yaml <<EOF
+dc.identifier: annonars/clinvar-$CLINVAR_RELEASE+$ANNONARS_VERSION
+dc.title: annonars ClinVar SV variant database
+dc.creator: NCBI ClinVar Team
+dc.contributor:
+  - VarFish Development Team
+dc.format: application/x-rocksdb
+dc.date: $(date +%Y%m%d)
+x-version: $CLINVAR_RELEASE+$ANNONARS_VERSION
+x-genome-release: $genome_release
+dc.description: |
+  RocksDB with SV information from ClinVar weekly release $CLINVAR_RELEASE,
+  created from clinvar-data-jsonl output of this release and annonars
+  v${ANNONARS_VERSION}.
+dc.source:
+  - PMID:29165669
+  - https://github.com/bihealth/clinvar-data-jsonl
+  - https://github.com/bihealth/annonars-data-clinvar
+x-created-from:
+  - name: ClinVar weekly release
+    version: $CLINVAR_RELEASE
+EOF
+
+ls -lhR $OUTPUT_DIR
+cat $OUTPUT_DIR/$genome_release/clinvar-sv/spec.yaml
